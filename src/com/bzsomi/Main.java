041dde31359fd9361ada936015af6c96ab26eef4
Zsombor <brozsek.zsombor@petrik.hu>
2020-10-06 15:24:20 +0200

    Fajl class kesz

---
+++ src/com/bzsomi/Main.java
@@ -0,0 +1,9 @@
+package com.bzsomi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Fajl szoveg = new Fajl("xxx","txt","01234567765432100");
+        System.out.println(szoveg);
+    }
+}
